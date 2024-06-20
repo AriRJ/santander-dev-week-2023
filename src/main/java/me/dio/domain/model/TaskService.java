cb00db6e88d651224c878f964ab5024c1a2c7451
Ari Alberto <154787080+AriRJ@users.noreply.github.com>
2024-06-20 01:25:14 -0300

    Update TaskService.java

--- src/main/java/me/dio/domain/model/TaskService.java
+++ src/main/java/me/dio/domain/model/TaskService.java
@@ -2,10 +2,14 @@ package me.dio.service;
 
 import me.dio.domain.model.Task;
 import me.dio.domain.repository.TaskRepository;
+import me.dio.dto.TaskDTO;
+import me.dio.exception.TaskNotFoundException;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskService {
@@ -16,19 +20,43 @@ public class TaskService {
         this.taskRepository = taskRepository;
     }
 
-    public List<Task> findAll() {
-        return taskRepository.findAll();
+    public List<TaskDTO> findAll() {
+        return taskRepository.findAll().stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
     }
 
-    public Task findById(Long id) {
-        return taskRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    public TaskDTO findById(Long id) {
+        Task task = taskRepository.findById(id).orElseThrow(() -> new TaskNotFoundException(id));
+        return convertToDTO(task);
     }
 
-    public Task save(Task task) {
-        return taskRepository.save(task);
+    public TaskDTO save(TaskDTO taskDTO) {
+        Task task = convertToEntity(taskDTO);
+        task.setCreatedAt(LocalDateTime.now());
+        return convertToDTO(taskRepository.save(task));
+    }
+
+    public TaskDTO update(Long id, TaskDTO taskDTO) {
+        Task existingTask = taskRepository.findById(id).orElseThrow(() -> new TaskNotFoundException(id));
+        BeanUtils.copyProperties(taskDTO, existingTask, "id", "createdAt");
+        existingTask.setCompletedAt(taskDTO.getCompletedAt());
+        return convertToDTO(taskRepository.save(existingTask));
     }
 
     public void deleteById(Long id) {
         taskRepository.deleteById(id);
+    }
+
+    private TaskDTO convertToDTO(Task task) {
+        TaskDTO taskDTO = new TaskDTO();
+        BeanUtils.copyProperties(task, taskDTO);
+        return taskDTO;
+    }
+
+    private Task convertToEntity(TaskDTO taskDTO) {
+        Task task = new Task();
+        BeanUtils.copyProperties(taskDTO, task);
+        return task;
     }
 }
