13facf50eacfb6f1edf99709909e348f51124059
Ari Alberto <154787080+AriRJ@users.noreply.github.com>
2024-06-20 01:24:52 -0300

    Update Task.java

--- src/main/java/me/dio/domain/model/Task.java
+++ src/main/java/me/dio/domain/model/Task.java
@@ -1,6 +1,7 @@
 package me.dio.domain.model;
 
 import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 
 @Entity(name = "tb_task")
@@ -10,23 +11,21 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 100)
     private String title;
+
+    @Column(length = 500)
     private String description;
 
-    @Column(name = "created_at")
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
     @Column(name = "completed_at")
     private LocalDateTime completedAt;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Status status;
 
     // Getters and Setters
-}
-
-enum Status {
-    PENDING,
-    IN_PROGRESS,
-    COMPLETED
 }
