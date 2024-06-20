e8f58f7eaf75d172c2e682aff18ea17f23aab0ab
Ari Alberto <154787080+AriRJ@users.noreply.github.com>
2024-06-20 01:23:44 -0300

    Create TaskService.java

---
+++ src/main/java/me/dio/domain/model/TaskService.java
@@ -0,0 +1,34 @@
+package me.dio.service;
+
+import me.dio.domain.model.Task;
+import me.dio.domain.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+    public Task findById(Long id) {
+        return taskRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    public Task save(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteById(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
